8194f5bacc82b6047226657ea986d37e8b858c55
Corey Williamson <euclidianAce@protonmail.com>
2025-08-13 15:32:40 -0500

    luarocks stuff

--- build-stuff.lua
+++ build-stuff.lua
@@ -1,4 +1,8 @@
-local file = assert(io.open("site/index.html", "w"))
+local lexical_path = require "lexical-path"
+local path = lexical_path.from_unix "site/index.html"
+
+
+local file = assert(io.open(path:to_string(), "w"))
 file:write [[
 <h1>hey</h1>
 <p>howdy</p>
